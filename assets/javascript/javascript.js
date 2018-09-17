44e0a3c440cd940faf652bb13d1588a3fc14ced8
Jobi Mathai <jobi.mathai@gmail.com>
2018-09-16 19:20:39 -0700

    fixed typo

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -24,7 +24,7 @@ $(document).ready(function () {
         num2 = Math.floor(Math.random() * 11 + 1);
         num3 = Math.floor(Math.random() * 11 + 1);
         num4 = Math.floor(Math.random() * 11 + 1);
-        userTotal = 0;
+        userScore = 0;
         $("#finalScore").text(userScore);
     }
 
